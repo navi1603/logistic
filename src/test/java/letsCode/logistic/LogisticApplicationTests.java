08eef57e293979e215238603f3a981ec69e70dc4
navi1603 <ukrainec1603@gmail.com>
2022-01-04 17:56:17 +0300

    First commit

---
+++ src/test/java/letsCode/logistic/LogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package letsCode.logistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
